dbbb4d61eb169a9ad6d720aae7c90c10fc32917d
Stefan Generalao <stegen@on.sinch.com>
2021-08-25 19:22:19 +0200

    Add "what is" command sourced from Urban Dictionary

--- src/addMessageCreateEvent.js
+++ src/addMessageCreateEvent.js
@@ -14,16 +14,31 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', async (msg) => {
-  const messageCreateFunction = messageCreateCallbacks[msg.content];
-  if (!messageCreateFunction) return;
-  const reply = await messageCreateFunction(msg);
+  const messageContent = msg.content.toLowerCase();
+
+  const messageCreateKey =
+    Object.keys(messageCreateCallbacks).find((key) => messageContent === key) ||
+    Object.keys(messageCreateCallbacks).find((key) =>
+      messageContent.startsWith(key)
+    );
+  const messageCreateCallback = messageCreateCallbacks[messageCreateKey];
+  if (!messageCreateCallback) return;
+
+  const reply = await messageCreateCallback(msg);
   if (!reply) return;
-  console.log(`Replying ${msg.author.username} with "${reply}".`);
-  msg.reply(reply);
+
+  console.log(`Replying ${msg.author.username} with "${reply}".\n`);
+
+  if (Array.isArray(reply)) {
+    reply.forEach((r) => msg.reply(r));
+  } else {
+    msg.reply(reply);
+  }
 });
 
 addMessageCreateEvent = (messageKey, callback) => {
-  const messageCreateCallback = messageCreateCallbacks[messageKey];
+  const messageCreateCallback =
+    messageCreateCallbacks[messageKey.toLowerCase()];
   if (messageCreateCallback) throw new error('Callback already exists.');
   messageCreateCallbacks[messageKey] = callback;
 };
