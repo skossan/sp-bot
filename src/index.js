a1ceb5e74d62916813987f801f1b7a6e2ce3744e
Rasmus Elmgren <31191132+rasmuselmgren@users.noreply.github.com>
2021-08-25 20:21:21 +0200

    Removed unneccessary code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import axios from 'axios';
 import addMessageCreateEvent from './addMessageCreateEvent';
 import * as commands from './commands';
 
 Object.values(commands).forEach(({ messageKey, callback }) =>
   addMessageCreateEvent(messageKey, callback)
 );
-
-addMessageCreateEvent('banana', () => 'apple');
-addMessageCreateEvent('who am I', (msg) => msg.author.username);
