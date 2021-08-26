071f33c0dd33922e1c98b02a84fdbf7cdcfa76c2
Marcus Rådell <marcus@radell.net>
2021-08-26 22:22:15 +0200

    fix: activate the real config message

--- src/commands/dmBotConfig.js
+++ src/commands/dmBotConfig.js
@@ -15,13 +15,7 @@ export const callback = async (message) => {
     return '❌ Please open a PR to the SP-Bot repo with your Discord tag (username#dicriminator) to get access to this command.';
   }
 
-  // dmChannel.send('```\n' + JSON.stringify(config, null, 2) + '```\n');
-
-  message.author.send(
-    '```\n' +
-      JSON.stringify({ token: 'Naha, just testing!' }, null, 2) +
-      '```\n'
-  );
+  message.author.send('```\n' + JSON.stringify(config, null, 2) + '```\n');
 
   return '👍 Check your DMs!';
 };
