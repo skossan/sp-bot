2b742ab5a5eb258cd85960165946523a854713d6
Marcus Rådell <marcus@radell.net>
2021-08-26 22:18:47 +0200

    fix: forgot some debug code and reverted to author.send

--- src/commands/dmBotConfig.js
+++ src/commands/dmBotConfig.js
@@ -15,18 +15,13 @@ export const callback = async (message) => {
     return '❌ Please open a PR to the SP-Bot repo with your Discord tag (username#dicriminator) to get access to this command.';
   }
 
-  const dmChannel = await message.author.createDM();
+  // dmChannel.send('```\n' + JSON.stringify(config, null, 2) + '```\n');
 
-  dmChannel.send('```\n' + JSON.stringify(config, null, 2) + '```\n');
+  message.author.send(
+    '```\n' +
+      JSON.stringify({ token: 'Naha, just testing!' }, null, 2) +
+      '```\n'
+  );
 
   return '👍 Check your DMs!';
 };
-
-const result = callback({
-  author: {
-    tag: allowList[0],
-    send: (sentMessage) => console.log({ sentMessage }),
-  },
-});
-
-console.log(result);
