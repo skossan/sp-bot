5dcc59f79f954776645699562306b3bd72069175
Rasmus Elmgren <31191132+rasmuselmgren@users.noreply.github.com>
2021-08-27 17:11:13 +0200

    changes the if statement syntax

---
+++ src/commands/commands.js
@@ -0,0 +1,9 @@
+export const messageKey = '!commands';
+
+export const callback = () =>
+  `These are the current available commands:\n
+  !banana\n
+  !joke\n
+  !pear\n
+  !what is\n
+  !who am i`;
