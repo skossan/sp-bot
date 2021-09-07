37e5bff69809c1e9c1f3e01609f549c19c0a17dc
Rasmus Elmgren <31191132+rasmuselmgren@users.noreply.github.com>
2021-09-07 19:11:27 +0200

    refactor the echo command to include the weather api key

--- src/commands/ts/token.ts
+++ src/commands/ts/token.ts
@@ -1,6 +1,6 @@
 import { Interaction } from 'discord.js';
 
-const { DEV_TOKEN, DEV_CLIENT_ID, GUILD_ID } = process.env;
+const { DEV_TOKEN, DEV_CLIENT_ID, GUILD_ID, WEATHER_API_KEY } = process.env;
 
 const name = 'token';
 
@@ -14,16 +14,12 @@ const callback = (interaction: Interaction) => {
       !Array.isArray(interaction.member.roles) &&
       interaction.member.roles.cache.some(
         (role) => role.name === 'sp-bot-developer'
-      );
+    );
 
     if (hasDeveloperPermission) {
       interaction.user.send(
-        `Execute the following command in the root of the project folder:\n\`\`\`echo "\\nDEV_TOKEN=${DEV_TOKEN} \\\n\\nGUILD_ID=${GUILD_ID} \\\n\\nDEV_CLIENT_ID=${DEV_CLIENT_ID}" >> .env\`\`\``
+        `Execute the following command in the root of the project folder:\n\`\`\`echo "\\nDEV_TOKEN=${DEV_TOKEN} \\\n\\nGUILD_ID=${GUILD_ID} \\\n\\nDEV_CLIENT_ID=${DEV_CLIENT_ID} \\\n\\nWEATHER_API_KEY=${WEATHER_API_KEY}" >> .env\`\`\``
       );
-      interaction.user.send(`Optional keys you can add to your .env file:
-*WEATHER_API_KEY=587b112b73d3331d7bbfd7247516e478*
-      `);
-
       return 'You will find your developer credentials in your DM 👍 ';
     } else {
       return `You don't seem to have the correct role. Please contact an admin if you want to work on the project and get developer credentials.`;
