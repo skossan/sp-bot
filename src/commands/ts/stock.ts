82e68ba47096d87c6401d5070e1251fa5514f9f1
Rasmus Elmgren <31191132+rasmuselmgren@users.noreply.github.com>
2021-10-12 20:09:33 +0200

    Added a simple stock command that returns the current proce of the chosen stock

---
+++ src/commands/ts/stock.ts
@@ -0,0 +1,55 @@
+import { ApplicationCommandOptionType } from 'discord-api-types/v9';
+import { Interaction } from 'discord.js';
+import { ICommand, IOption } from '../commands';
+import axios from 'axios';
+import puppeteer from 'puppeteer';
+
+const { STOCK_API_KEY } = process.env;
+
+const name = 'stock';
+
+const description = 'Responds with the value of the chosen stock';
+
+const callback = async (interaction: Interaction) => {
+  try {
+    if (!interaction.isCommand()) throw new Error('Not a command.');
+
+    const term = interaction.options.get('word')!.value;
+
+    if (!term) {
+      return 'Write a stock name after "stock". For example: "stock tesla".';
+    }
+
+    const url = `https://www.avanza.se/_cqbe/search/global-search/global-search-template?query=${term}`;
+
+    const data = await axios.get(url);
+
+    const name = data.data.resultGroups[0].hits[0].link.linkDisplay;
+    const price = data.data.resultGroups[0].hits[0].lastPrice;
+    const currency = data.data.resultGroups[0].hits[0].currency;
+
+    const response = `${name} ${price} ${currency}`;
+
+    return response;
+  } catch (e) {
+    return `Something went wrong. Sorry 😢`;
+  }
+};
+
+const options: IOption[] = [
+  {
+    type: ApplicationCommandOptionType.String,
+    name: 'word',
+    description: 'What is this word?',
+    required: true,
+  },
+];
+
+const stock = {
+  callback,
+  description,
+  name,
+  options,
+};
+
+export default stock;
